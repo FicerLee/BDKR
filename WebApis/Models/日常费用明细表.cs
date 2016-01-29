ed647038190c980de9c7e4862d3ac16df039e35e
ficerlee <ficerlee@qq.com>
2016-01-29 15:59:13 +0800

    sssa

--- WebApis/Models/日常费用明细表.cs
+++ WebApis/Models/日常费用明细表.cs
@@ -11,7 +11,17 @@ namespace WebApis.Models
         [Key]
         public string 编码 { get; set; }
         public string 日常费用表编码 { get; set; }
+        public decimal 薪资 { get; set; }
+        public decimal 劳动保护费 { get; set; }
+        public decimal 职工培训费 { get; set; }
+        public decimal 职工福利费 { get; set; }
+        public decimal 工会经费 { get; set; }
+        public decimal 其他费用 { get; set; }
+
+
 
         public virtual 日常费用表 日常费用表 { get; set; }
+
+        
     }
 }
